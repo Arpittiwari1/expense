c3db289fbe183f59b7a92cc9f38155d03e10883f
Arpittiwari1 <arpittiwari0408+1@gmail.com>
2025-06-01 20:41:27 +0530

    fixing name issue

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import AuthLayout from '../../components/layouts/authLayout';
+import AuthLayout from '../../components/layouts/AuthLayout'
 import { useState } from 'react';
 
 import { Link, useNavigate } from "react-router-dom";
